2d5c17be019e4412a28ec0db623fa31250805bce
gitSaiev <saievmuhammad@mail.ru>
2024-08-04 18:19:02 +0300

    Init

---
+++ src/main/java/ru/saiev/netristask/NetrisTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.saiev.netristask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetrisTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetrisTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/saiev/netristask/NetrisTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.saiev.netristask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetrisTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
